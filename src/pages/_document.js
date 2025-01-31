e328f1cd9c404d8a143b0a8a88f58a534ead94ac
wipaweenan <nanwipawee2545@gmail.com>
2025-02-01 00:39:06 +0700

    init first

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet"></link>
       <Head />
       <body className="antialiased">
         <Main />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,114 +1,90 @@
+import React from "react";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Photo from "../../public/image-equilibrium.jpg";
+import Avatar from "../../public/image-avatar.png";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function Home() {
+const Nft = () => {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="w-full">
+      <div className=" flex flex-col bg-[#15273f] p-4 rounded-lg px-4 drop-shadow-2xl">
+        {/* container photo */}
+        <div className="relative w-64 h-64 group cursor-pointer">
+          <Image
+            className="w-64 h-64 rounded-lg transition-all duration-300 
+                   group-hover:opacity-50"
+            src={Photo}
+            alt="Photo NFT"
+          />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <div
+            className="absolute bg-[#00fff77d] rounded-lg inset-0 flex items-center justify-center opacity-0 
+                    group-hover:opacity-100 transition-opacity duration-300"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <svg width="48" height="48" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fillRule="evenodd">
+                <path d="M0 0h48v48H0z" />
+                <path
+                  d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15Zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10Zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6Z"
+                  fill="#FFF"
+                  fillRule="nonzero"
+                />
+              </g>
+            </svg>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <span className="text-white font-semibold text-lg pt-3 pb-3 cursor-pointer hover:text-[#00fff7] duration-300">
+          Equilibrium#3420
+        </span>
+        <span className="w-64 text-[#8bacda] font-light pb-4">
+          Our Equilibrium collection promotes balance and calm.
+        </span>
+        <div className="flex w-full justify-between font-semibold text-sm ">
+          <div className=" flex text-[#00fff7]">
+            <svg
+              className="mr-2"
+              width="11"
+              height="18"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11 10.216 5.5 18 0 10.216l5.5 3.263 5.5-3.262ZM5.5 0l5.496 9.169L5.5 12.43 0 9.17 5.5 0Z"
+                fill="#00FFF8"
+              />
+            </svg>
+            0.041 ETH
+          </div>
+
+          <div className=" flex text-[#8BACD9] pb-4">
+            <svg
+              className="mr-2"
+              width="17"
+              height="17"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.305 2.007a6.667 6.667 0 1 0 0 13.334 6.667 6.667 0 0 0 0-13.334Zm2.667 7.334H8.305a.667.667 0 0 1-.667-.667V6.007a.667.667 0 0 1 1.334 0v2h2a.667.667 0 0 1 0 1.334Z"
+                fill="#8BACD9 "
+              />
+            </svg>
+            3 days left
+          </div>
+        </div>
+        <div className="top-1/2 h-[0.5px] w-full -translate-y-1/2 transform bg-[#8BACD9]"></div>
+        <div className="flex w-64 pt-4  text-sm">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            className="w-7 h-7 mr-2 border border-white rounded-full"
+            src={Avatar}
+            alt="Avatar"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <span className="text-[#8bacda] pr-1"> Creation of</span>
+          <span className="font-medium text-white cursor-pointer hover:text-[#00fff7] duration-300">
+            {" "}
+            Jules Wyvern
+          </span>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Nft;
